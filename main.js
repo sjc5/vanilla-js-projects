032d888ca9dccb2de215710a813a87f23103e1d1
Active Fog <email@sjc.sh>
2021-06-24 17:34:04 -0500

    Finish palindrome tester and add styles.

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let Phrase = require("activefog-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+  event.preventDefault();
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.content == "") {
+    palindromeResult.innerHTML = (`<p>Need ideas? Try "<b>Dennis and Edna sinned.</b>"</p>`)
+  } else if (phrase.palindrome()) {
+    palindromeResult.innerHTML = (`<p>"<b>${phrase.content}</b>" is a palindrome!</p><h1>😎</h1>`);
+  } else {
+    palindromeResult.innerHTML = (`<p>"<b>${phrase.content}</b>" is not a palindrome.</p><h1>😑</h1>`)
+  }
 }
+
+let button = document.querySelector("#palindromeTester");
+button.addEventListener("submit", function() {
+  palindromeTester(event);
+})
